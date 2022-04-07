81bc1014b057f40a95a95151e46361038961dc12
Nommiiee <kunalkashyp3@gmail.com>
2022-04-07 15:38:43 +0530

    alert removed

--- src/js/testGame.js
+++ src/js/testGame.js
@@ -1,4 +1,5 @@
 let player = document.getElementById("player");
+let boundary = document.getElementById("boundary");
 
 document.onmousedown = function (e) {
   playerMove(e);
@@ -6,15 +7,14 @@ document.onmousedown = function (e) {
 };
 
 document.onmouseup = function () {
-  alert("Game Over");
-  location.reload();
-
+  // alert("Game Over");
+  // location.reload();
   document.onmousemove = null;
 };
 
 function playerMove(e) {
-  let x = e.clientX;
-  let y = e.clientY;
+  let x = e.clientX - player.offsetWidth / 2;
+  let y = e.clientY - player.offsetHeight / 2;
   player.style.left = x + "px";
   player.style.top = y + "px";
   if (x >= window.innerWidth - player.offsetWidth) {
@@ -27,32 +27,60 @@ function playerMove(e) {
 
 // touch
 
+document.ontouchstart = function (e) {
+  touchPlayerMove(e);
+  document.ontouchmove = touchPlayerMove;
+};
+
+document.ontouchend = function (e) {
+  // alert("Game Over");
+  // location.reload();
+  document.ontouchmove = null;
+};
+
+function touchPlayerMove(e) {
+  let x = e.changedTouches[0].clientX - player.offsetWidth / 2;
+  let y = e.changedTouches[0].clientY - player.offsetHeight / 2;
+
+  player.style.left = x + "px";
+  player.style.top = y + "px";
+  if (x >= window.innerWidth - player.offsetWidth) {
+    player.style.left = window.innerWidth - player.offsetWidth + "px";
+  }
+  if (y >= window.innerHeight - player.offsetHeight) {
+    player.style.top = window.innerHeight - player.offsetHeight + "px";
+  }
+}
+
+// Enemy Element And Movement
 let enemy = document.getElementsByClassName(`enemy`);
+
 Array.from(enemy).forEach((element) => {
+  let gameState = true;
   let x = Math.trunc(Math.random() * innerWidth - element.clientWidth);
   let y = Math.trunc(Math.random() * innerHeight - element.clientHeight);
 
-  element.style.left = x + "px";
-  element.style.top = y + "px";
   let vFlag = true;
   let hFlag = true;
 
-  player.style.position = "absolute";
   player.style.left = "0px";
   player.style.top = "0px";
 
-  function movement() {
-    let enemyPos = element.getBoundingClientRect();
-    let playerPos = player.getBoundingClientRect();
+  let gameMove = setInterval(movement, 1);
 
+  let speed = 1;
+
+  function movement() {
+    let playerPos = player.getBoundingClientRect();
+    let enemyPos = element.getBoundingClientRect();
     if (vFlag) {
-      x += 1;
+      x += speed;
       element.style.left = x + "px";
       if (x >= window.innerWidth - element.clientWidth) {
         vFlag = false;
       }
     } else if (!vFlag) {
-      x -= 1;
+      x -= speed;
       element.style.left = x + "px";
       if (x <= 0) {
         vFlag = true;
@@ -60,13 +88,13 @@ Array.from(enemy).forEach((element) => {
     }
 
     if (hFlag) {
-      y += 1;
+      y += speed;
       element.style.top = y + "px";
       if (y >= window.innerHeight - element.clientHeight) {
         hFlag = false;
       }
     } else if (!hFlag) {
-      y -= 1;
+      y -= speed;
       element.style.top = y + "px";
       if (y <= 0) {
         hFlag = true;
@@ -81,8 +109,12 @@ Array.from(enemy).forEach((element) => {
     ) {
       alert("Game Over");
       location.reload();
+      gameState = false;
+      clearInterval(gameMove);
     }
   }
 
-  setInterval(movement, 1);
+  let changeSpeed = setInterval((change) => {
+    speed += 1;
+  }, 5000);
 });
