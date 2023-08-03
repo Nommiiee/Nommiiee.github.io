48fb6e27f805dccbebf0a300dadcaf63c523f524
Nommiiee <kunalkashyp3@gmail.com>
2023-08-03 16:43:18 +0530

    added Vrchat avatar pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./**/**/*.{html,js,css}",
     "index.html",
     "portfolio/src/js/index.js",
     "portfolio/src/css/style.css",
