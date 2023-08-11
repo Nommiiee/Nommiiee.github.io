889427c4d685deaa591ff1c25ef1124b38ea6696
Nommiiee <kunalkashyp3@gmail.com>
2023-08-11 21:08:29 +0530

    reworkd Nom's Bar Website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./**/**/*.{html,js,css}",
-    "index.html",
-    "portfolio/src/js/index.js",
-    "portfolio/src/css/style.css",
-  ],
+  content: ["./**/**/*.{html,js,css}"],
   theme: {
     extend: {},
   },
